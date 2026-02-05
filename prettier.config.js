3f7cb67225652f3c37a9018588513954b70dcd97
Syed Danish Hassan <sdanishhassan@gmail.com>
2026-02-05 20:30:08 +0500

    Tailwind installed and prettier configuration files added

---
+++ prettier.config.js
@@ -0,0 +1,66 @@
+// // prettier.config.js
+
+// /* @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+// module.exports = {
+//   plugins: [require("prettier-plugin-tailwindcss")],
+//   singleQuote: true,
+// };
+
+// // prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+// // /**
+// //  * @see https://prettier.io/docs/configuration
+// //  * @type {import("prettier").Config}
+// //  */
+// // const config = {
+// //   trailingComma: "es5",
+// //   tabWidth: 4,
+// //   semi: false,
+// //   singleQuote: true,
+// // };
+
+// // module.exports = config;
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+// /**
+//  * @see https://prettier.io/docs/configuration
+//  * @type {import("prettier").Config}
+//  */
+// const config = {
+//   plugins: ["prettier-plugin-tailwindcss"],
+//   trailingComma: "es5",
+//   tabWidth: 4,
+//   semi: false,
+//   singleQuote: true,
+// };
+
+// export default config;
+
+// // prettier.config.js, .prettierrc.js, prettier.config.cjs, or .prettierrc.cjs
+
+// /**
+//  * @see https://prettier.io/docs/configuration
+//  * @type {import("prettier").Config}
+//  */
+// const config = {
+//   trailingComma: "es5",
+//   tabWidth: 4,
+//   semi: false,
+//   singleQuote: true,
+// };
+
+// module.exports = config;
+// const config = {
+//   plugins: [require("prettier-plugin-tailwindcss")],
+//   singleQuote: true,
+// };
+
+// module.exports = config;
+
+// prettier.config.js
+
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default {
+    plugins: ['prettier-plugin-tailwindcss'],
+    singleQuote: true,
+}
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,13 @@
 function Home() {
-  return (
-    <div>
-      <h1>
-        The best pizza.
-        <br />
-        Straight out of the oven, straight to you.
-      </h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1 className=" font-semibold text-center file:text-xl text-yellow-500  ">
+                The best pizza.
+                <br />
+                Straight out of the oven, straight to you.
+            </h1>
+        </div>
+    )
 }
 
-export default Home;
+export default Home
