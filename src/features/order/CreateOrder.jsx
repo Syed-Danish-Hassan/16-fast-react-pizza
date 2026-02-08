83007211e69bab39c3a6a5f92d2e0fc27f3ad231
Syed Danish Hassan <sdanishhassan@gmail.com>
2026-02-08 13:23:35 +0500

    styling order page text inputs   check box buttons, and apply  rules for common components

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,6 @@
 //import { useState } from "react";
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -11,21 +11,21 @@ const isValidPhone = (str) =>
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -34,7 +34,7 @@ const fakeCart = [
 
 function CreateOrder() {
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
   console.log(formErrors);
 
@@ -50,13 +50,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required className="input" />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input" />
           </div>
           {formErrors?.phone && <p className="error">{formErrors.phone}</p>}
         </div>
@@ -64,7 +64,13 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              required
+              //className="w-full rounded-full border border-stone-200 px-4 py-2 transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+              className="input"
+            />
           </div>
         </div>
 
@@ -73,6 +79,7 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 border border-stone-200 px-4 py-2 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
@@ -80,8 +87,11 @@ function CreateOrder() {
         </div>
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
         <div>
-          <button disabled={isSubmitting}>
-            {isSubmitting ? "Placing order..." : "Order now"}
+          <button
+            disabled={isSubmitting}
+            className="upercase inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold tracking-wide text-stone-800 outline-none transition-colors duration-300 ease-in-out hover:bg-yellow-300 focus:ring focus:ring-yellow-300 focus:ring-offset-2 active:bg-slate-400"
+          >
+            {isSubmitting ? 'Placing order...' : 'Order now'}
           </button>
         </div>
       </Form>
@@ -97,29 +107,31 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on" ? true : false,
+    priority: data.priority === 'on' ? true : false,
   };
 
   const errors = {};
   if (!isValidPhone(data.phone)) {
-    errors.phone = "Phone number is not valid.We might need it to contact you";
+    errors.phone = 'Phone number is not valid.We might need it to contact you';
   }
   if (Object.keys(errors).length > 0) {
     return errors;
   }
   console.log(order);
-  ///if everything is okay  then create order and redirect
-  const newOrder = await createOrder(order);
+  // ///if everything is okay  then create order and redirect
+  // const newOrder = await createOrder(order);
 
-  // Validate the data
-  // if (!data.customer || data.customer.length < 2) {
-  //   return { error: "Customer name is too short" };
-  // }
-  // if (!isValidPhone(data.phone)) {
-  //   return { error: "Phone number is not valid" };
-  // }
+  // // Validate the data
+  // // if (!data.customer || data.customer.length < 2) {
+  // //   return { error: "Customer name is too short" };
+  // // }
+  // // if (!isValidPhone(data.phone)) {
+  // //   return { error: "Phone number is not valid" };
+  // // }
 
-  return redirect(`/order/${newOrder.id}`);
+  // return redirect(`/order/${newOrder.id}`);
+
+  return null;
 }
 
 export default CreateOrder;
