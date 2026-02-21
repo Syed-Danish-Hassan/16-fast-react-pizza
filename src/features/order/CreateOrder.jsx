e723a598577e53f88dafbc929d2b2191c532314a
Syed Danish Hassan <sdanishhassan@gmail.com>
2026-02-21 22:43:28 +0500

    redux was added store class was added

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,7 +2,7 @@
 import { Form, useActionData, useNavigation } from 'react-router-dom';
 //import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
-
+import { useSelector } from 'react-redux';
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
@@ -37,6 +37,8 @@ function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   const formErrors = useActionData();
+  const username = useSelector((state) => state.user.username);
+
   console.log(formErrors);
 
   //const navigate = useNavigate();
@@ -51,7 +53,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" required className="input grow" />
+          <input
+            type="text"
+            name="customer"
+            required
+            className="input grow"
+            defaultValue={username}
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
